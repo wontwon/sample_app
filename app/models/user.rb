e23b5e8ca23606e0539deb4d0e2bbee9e15ee21b
Tuan Duong <tuanduong15@gmail.com>
2013-12-31 02:22:48 -0500

    added user sign-in and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  before_save {self.email = email.downcase}
+  before_create :create_remember_token
+  
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
@@ -6,4 +9,20 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum: 6}
   validates :password_confirmation, presence: true
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
 end
